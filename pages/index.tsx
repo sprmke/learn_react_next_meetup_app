489e489b7aec2d706030b90ec0108b1f49053830
Michael Manlulu <michaeldmanlulu@gmail.com>
2022-09-22 12:02:23 +0800

    330. outputting a list of meetups

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,25 @@
+import MeetupList from '../components/meetups/MeetupList';
+import { MeetupInfo } from '../types/meetup';
+
+const DUMMY_MEETUPS: MeetupInfo[] = [
+  {
+    id: 'm1',
+    title: 'Meetup 2',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Restaurant_N%C3%A4sinneula.jpg/1280px-Restaurant_N%C3%A4sinneula.jpg',
+    address: 'Meetup1 Address',
+  },
+  {
+    id: 'm2',
+    title: 'Meetup 2',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Restaurant_N%C3%A4sinneula.jpg/1280px-Restaurant_N%C3%A4sinneula.jpg',
+    address: 'Meetup2 Address',
+  },
+];
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
 };
 
 export default HomePage;
