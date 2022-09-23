a6fecb1a87620bb9753ef99187d8a2078fcd3bec
Michael Manlulu <michaeldmanlulu@gmail.com>
2022-09-23 11:04:05 +0800

    340. working with params for SSG data fetching

--- pages/[meetupId]/index.tsx
+++ pages/[meetupId]/index.tsx
@@ -1,14 +1,48 @@
+import { GetStaticProps, GetStaticPropsContext } from 'next';
 import MeetupDetail from '../../components/meetups/MeetupDetail';
+import { MeetupInfo } from '../../types/meetup';
 
-const MeetupDetailsPage = () => {
+interface StaticPropsType {
+  meetupData: MeetupInfo;
+}
+
+type ParamsType = {
+  meetupId: string;
+};
+
+const MeetupDetailsPage = ({
+  title,
+  description,
+  address,
+  image,
+}: MeetupInfo) => {
   return (
     <MeetupDetail
-      title='Meetup 1'
-      description='Meetup1 Description'
-      address='Meetup1 Address'
-      image='https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Restaurant_N%C3%A4sinneula.jpg/1280px-Restaurant_N%C3%A4sinneula.jpg'
+      title={title}
+      description={description}
+      address={address}
+      image={image}
     />
   );
+};
+
+export const getStaticProps: GetStaticProps<StaticPropsType> = ({
+  params,
+}: GetStaticPropsContext<ParamsType>) => {
+  const { meetupId } = params;
+  // fetch api here
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        title: 'Meetup 1',
+        description: 'Meetup1 Description',
+        address: 'Meetup1 Address',
+        image:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Restaurant_N%C3%A4sinneula.jpg/1280px-Restaurant_N%C3%A4sinneula.jpg',
+      },
+    },
+  };
 };
 
 export default MeetupDetailsPage;
