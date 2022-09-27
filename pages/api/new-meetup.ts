dd9a31274bf99881399983e6948c7ac82744f8f4
Michael Manlulu <michaeldmanlulu@gmail.com>
2022-09-28 00:55:35 +0800

    343. working with mongodb

--- pages/api/new-meetup.ts
+++ pages/api/new-meetup.ts
@@ -1,10 +1,32 @@
+import { MongoClient } from 'mongodb';
+
 // POST /api/new-meetup
 
-const handler = (req, res) => {
+const handler = async (req, res) => {
   if (req.method === 'POST') {
-    const data = req.body;
-    const { title, image, address, description } = data;
-    // store data in the database
+    try {
+      const data = req.body;
+
+      const MONGODB_CONNECTION_STRING =
+        process.env.REACT_APP_MONGODB_CONNECTION_STRING ?? '';
+
+      // connect to database
+      const client = await MongoClient.connect(MONGODB_CONNECTION_STRING);
+      const db = client.db();
+
+      // get the meetups collection and insert meetup form data
+      const meetupsCollection = db.collection('meetups');
+      const result = await meetupsCollection.insertOne(data);
+      console.log('result', result);
+
+      // close the database connection
+      client.close();
+
+      // return a response with status and json message data
+      res.status(201).json({ message: 'Meetup Inserted' });
+    } catch (error) {
+      console.log('error', error);
+    }
   }
 };
 
