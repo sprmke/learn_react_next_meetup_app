076c7383eb873f029ee13c7653ba905e4eab9caf
Michael Manlulu <michaeldmanlulu@gmail.com>
2022-09-28 01:06:00 +0800

    344. sending HTTP requests to our API routes

--- pages/new-meetup/index.tsx
+++ pages/new-meetup/index.tsx
@@ -1,9 +1,21 @@
+import { useRouter } from 'next/router';
 import NewMeetupForm from '../../components/meetups/NewMeetupForm';
 import { MeetupInfo } from '../../types/meetup';
 
 const NewMeetupPage = () => {
-  const addMeetupHandler = (meetupInfo: MeetupInfo) => {
-    console.log('meetupInfo::', meetupInfo);
+  const router = useRouter();
+
+  const addMeetupHandler = async (meetupInfo: MeetupInfo) => {
+    const response = await fetch('/api/new-meetup', {
+      method: 'POST',
+      body: JSON.stringify(meetupInfo),
+      headers: {
+        'Content-type': 'application/json',
+      },
+    });
+    const data = await response.json();
+    console.log('data', data);
+    router.push('/');
   };
 
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
